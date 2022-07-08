0fabe214d1997226df18b8e9313cbb40d0afb207
anmol09988 <107464613+anmol09988@users.noreply.github.com>
2022-07-08 10:58:05 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -99,15 +99,15 @@ exports.execute = function (req, res) {
 
     const accountSid = requestBody.accountSid;
     const authToken = requestBody.authToken;
-    const to = requestBody.to;
-    const from = requestBody.messagingService;
-    const body = requestBody.body;   
-    console.log("requestBody: "+requestBody);
+//     const to = requestBody.to;
+//     const from = requestBody.messagingService;
+//     const body = requestBody.body;   
+//     console.log("requestBody: "+requestBody);
     console.log("ExecutedaccountSid: "+accountSid);
     console.log("ExecutedauthToken: "+authToken);
-    console.log("Executedto: "+to);
-    console.log("Executedfrom: "+from);
-    console.log("Executedbody: "+body);    
+//     console.log("Executedto: "+to);
+//     console.log("Executedfrom: "+from);
+//     console.log("Executedbody: "+body);    
 
 //     const client = require('twilio')(accountSid, authToken);
 //        client.messages
