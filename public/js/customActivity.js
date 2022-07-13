968bd28bf921e36675e899c122f4e220e7d82f09
anmol09988 <107464613+anmol09988@users.noreply.github.com>
2022-07-13 13:01:18 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -104,7 +104,7 @@ define([
             "authToken": authToken,
             "messagingService": messagingService,
             "body": body,
-            "to": "{{Event.DEAudience-0a845042-fe54-8ac9-88a5-b9947cda7676.PhoneNo}}" //<----This should map to your data extension name and phone number column
+            "to": "{{Event.DEAudience-0d565f3b-6f54-d93a-05ec-a3d4521b4166.PhoneNo}}" //<----This should map to your data extension name and phone number column
         }];
 
         payload['metaData'].isConfigured = true;
