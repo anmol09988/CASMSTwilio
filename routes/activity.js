c4c56ae2b0499c98d1c63dd4fbf6729484660a2e
anmol09988 <107464613+anmol09988@users.noreply.github.com>
2022-07-27 18:13:34 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -183,7 +183,7 @@ exports.validate = function (req, res) {
     console.log("3");
     console.log("2");
     console.log("1");
-    console.log("Validated: "+req.body.inArguments[0]);   
+   // console.log("Validated: "+req.body.inArguments[0]);   
 
     // Data from the req and put it in an array accessible to the main app.
     //console.log( req.body );
